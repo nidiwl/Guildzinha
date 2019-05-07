711839e7d843ba907dbf256d22d1caeedf03b4a3
Willyam Luccas <willyamluccas@gmail.com>
2019-05-07 13:34:56 -0300

    tirei o ant sleep

--- src/index.js
+++ src/index.js
@@ -43,10 +43,7 @@ bot.on('ready', () => {
   logger.info('Connected');
   logger.info(`Logged in as: ${bot.user.username} - (${bot.user.id})`);
 
-  var http = require("http");
-  setInterval(function() {
-      http.get("https://mibr-killboard.herokuapp.com");
-  }, 300000); // every 5 minutes (300000)
+
 
   if (config.discord.statusChannelId) {
     checkServerStatus();
