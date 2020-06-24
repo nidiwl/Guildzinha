02e6b5fabe76e9c96c188f4635b0534f9c458125
Willyam Luccas <willyamluccas@gmail.com>
2020-06-24 04:02:51 -0300

    ping;pong

--- src/index.js
+++ src/index.js
@@ -361,34 +361,15 @@ bot.on('message', msg => {
       });
       break;
   }
-
-  // Extract the required classes from the discord.js module
-const { Client, MessageEmbed } = require('discord.js');
-
-// Create an instance of a Discord client
-const client = new Client();
-
-/**
- * The ready event is vital, it means that only _after_ this will your bot start reacting to information
- * received from Discord
- */
-
-  // If the message is "how to embed"
-  if (message.content === 'how to embed') {
-    // We can create embeds using the MessageEmbed constructor
-    // Read more about all that you can do with the constructor
-    // over at https://discord.js.org/#/docs/main/master/class/MessageEmbed
-    const embed = new MessageEmbed()
-      // Set the title of the field
-      .setTitle('A slick little embed')
-      // Set the color of the embed
-      .setColor(0xff0000)
-      // Set the main content of the embed
-      .setDescription('Hello, this is a slick embed!');
-    // Send the embed to the same channel as the message
-    message.channel.send(embed);
-  }
-
 });
 
+  bot.on('message', msg => {
+    client.on('message', message => {
+      // If the message is "ping"
+      if (message.content === 'ping') {
+        // Send "pong" to the same channel
+        message.channel.send('pong');
+      }
+  });
+}
 bot.login(config.discord.token);
